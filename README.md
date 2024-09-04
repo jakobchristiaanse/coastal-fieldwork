5ffffcf886b284217b007f1fc8476950b42ca1ad
jakobchristiaanse <jakob.christiaanse@live.de>
2024-09-04 11:05:11 +0200

    ADD: Repository Files

--- README.md
+++ README.md
@@ -8,12 +8,6 @@ Welcome to coastal-fieldwork. This repository contains python code to read and p
 ## Usage
 The code allows users to read and process raw data files from the above instruments through classes and methods contained in the file cls_fieldwork.py. Further instructions on the use of the classes and methods is provided through comments in the code.
 
-## Questions
-If you have a question about the installation process or notebooks, feel free to open an
-issue in the [GitHub repository](https://github.com/jakobchristiaanse/coastal-fieldwork). If
-that's your first time, have a look at [these
-instructions](https://docs.github.com/en/issues/tracking-your-work-with-issues/creating-an-issue).
-
 ## Author
 Jakob C. Christiaanse
 [<img src=https://info.orcid.org/wp-content/uploads/2020/12/orcid_16x16.gif alt="ORCiD" width="16" height="16">](https://orcid.org/0009-0007-4089-3578)
--- setup.py
+++ setup.py
@@ -13,17 +13,24 @@ setup(
     version='1.0',
     author='Jakob C. Christiaanse',
     author_email='J.C.Christiaanse@tudelft.nl',
-    description='Pyhton based readout and processing of coastal fieldwork data',
+    description='Python based readout and processing of coastal fieldwork data',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=['src'],
     license='Apache-2.0',
-    keywords=[],
+    keywords=['Coastal Engineering', 'Field Work', 'Data Processing'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
-    install_requires=['numpy'],
+    install_requires=[
+        'numpy',
+        'pandas>=2.0.3',
+        'scipy',
+        'pyrsktools',
+        'tqdm',
+        'pyproj',
+        'matplotlib'],
     python_requires='>=3.7'
 )
