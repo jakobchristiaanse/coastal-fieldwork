3636cea239b5421f01cedf2304bd19de8d64e982
jakobchristiaanse <jakob.christiaanse@live.de>
2024-09-04 15:19:23 +0200

    ADD: __init__.py file in src folder

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ __all__ = ['src']
 
 __version__ = '1.0'
 __author__ = 'Jakob C. Christiaanse'
-__description__ = 'Pyhton based readout and processing of coastal fieldwork data'
+__description__ = 'Python based readout and processing of coastal fieldwork data'
